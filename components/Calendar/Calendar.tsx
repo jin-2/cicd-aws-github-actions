b2daff8a4b966b03140464024483687ebd857790
sujin2 <jmakeaz@gmail.com>
2024-12-11 19:05:01 +0900

    feat: Add current year, month

--- components/Calendar/Calendar.tsx
+++ components/Calendar/Calendar.tsx
@@ -5,6 +5,7 @@ import classNames from "classnames/bind";
 import Day from "@/components/Day/Day";
 import { useState } from "react";
 import { getMonthByWeek } from "@/utils/calendar";
+import { format } from "date-fns";
 
 const cx = classNames.bind(styles);
 
@@ -14,7 +15,7 @@ export default function Calendar() {
 
   return (
     <div className={cx("calendar")}>
-      <div className={cx("calendar-header")}></div>
+      <div className={cx("calendar-header")}>{format(now, "yyyy년 MM월")}</div>
       <div className={cx("calendar-body")}>
         {days.map((date) => (
           <Day key={date} date={date} />
